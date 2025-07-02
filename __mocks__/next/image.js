910fa3b2f25fa55068632aeec73b932a71280f60
david-rosval <20b3010135@untels.edu.pe>
2025-07-02 12:10:59 -0500

    chore: exclude priority attribute from img

--- __mocks__/next/image.js
+++ __mocks__/next/image.js
@@ -1,4 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // __mocks__/next/image.js
-export default function Image(props) {
+export default function Image({ priority, ...props}) {
   return <img {...props} />;
 }
