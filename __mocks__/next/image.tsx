021883dcdb0798781be1a1b2490de82ff34d8f4a
david-rosval <20b3010135@untels.edu.pe>
2025-07-03 17:21:50 -0500

    chore: add data-fill attribute to the mock of the image

--- __mocks__/next/image.tsx
+++ __mocks__/next/image.tsx
@@ -5,7 +5,8 @@ export default function Image({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   priority,
   unoptimized,
+  fill,
   ...props
-}: React.ImgHTMLAttributes<HTMLImageElement> & { unoptimized?: boolean; priority?: boolean }) {
-  return <img data-unoptimized={unoptimized?.toString()} {...props} />;
+}: React.ImgHTMLAttributes<HTMLImageElement> & { unoptimized?: boolean; priority?: boolean, fill?: boolean }) {
+  return <img data-unoptimized={unoptimized?.toString()} data-fill={fill?.toString()} {...props} />;
 }
