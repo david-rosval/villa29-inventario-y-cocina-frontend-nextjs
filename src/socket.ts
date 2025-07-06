18db8313b092d48ba2d6b7dfe8034b671d90b867
david-rosval <20b3010135@untels.edu.pe>
2025-07-06 13:33:06 -0500

    chore: configure socket connection

--- src/socket.ts
+++ src/socket.ts
@@ -4,4 +4,11 @@ import { io } from 'socket.io-client'
 
 const socketServerUrl = (process.env.NEXT_PUBLIC_MODE && process.env.NEXT_PUBLIC_MODE === 'dev') ? process.env.NEXT_PUBLIC_SOCKET_SERVER_URL_DEV : process.env.NEXT_PUBLIC_SOCKET_SERVER_URL_PROD;
 
-export const socket = io(socketServerUrl)
+export const socket = io(socketServerUrl, {
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+  timeout: 5000,
+  transports: ['websocket'], // asegura uso de websocket puro
+  withCredentials: true
+})
